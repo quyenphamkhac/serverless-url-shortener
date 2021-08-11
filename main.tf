1745fb19a8f771dcbdb3c9533e3092d4b1fb8ee4
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-08-11 17:11:36 +0700

    feat: update gitignore

--- main.tf
+++ main.tf
@@ -3,13 +3,3 @@ provider "aws" {
   shared_credentials_file = "/Users/mac/.aws/credentials"
   profile = "skg"
 }
-
-resource "aws_apigatewayv2_api" "shortener" {
-  name = "shortener-api"
-  protocol_type = "HTTP"
-}
-
-resource "aws_apigatewayv2_stage" "stage" {
-  id = aws_apigatewayv2_api.shortener.id
-  name = "dev"
-}
